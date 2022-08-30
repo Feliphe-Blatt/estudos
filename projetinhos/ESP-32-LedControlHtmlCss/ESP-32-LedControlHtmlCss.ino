eaa43eef1b689452f327fd1aa3f777b5444af237
Feliphe Sousa Blatt <felipheblatt@gmail.com>
2022-08-30 03:03:52 -0300

    Update ESP-32-LedControlHtmlCss.ino

--- projetinhos/ESP-32-LedControlHtmlCss/ESP-32-LedControlHtmlCss.ino
+++ projetinhos/ESP-32-LedControlHtmlCss/ESP-32-LedControlHtmlCss.ino
@@ -1,22 +1,26 @@
 #include <WiFi.h>
 #include <WebServer.h>
-#include "A1.h"   // Arquivo HTML + CSS
+#include "A1.h"   // Arquivo HTML-CSS-JS
 
-// Caso queira utilizar somente a rede do ESP-32 comente o 'define' abaixo:
-//#define USE_INTRANET
+// Caso queira utilizar somente a rede interna do ESP-32 comente o 'define' abaixo:
+#define USE_INTRANET
 
 // Rede interna ESP-32
 #define AP_SSID "Esp-32"
 #define AP_PASS "doumaooito"
 
 // Rede local pro ESP-32 se conectar
-#define LOCAL_SSID "-----"
-#define LOCAL_PASS "-----"
+#define LOCAL_SSID "SucoDeFruta"
+#define LOCAL_PASS "qcisqcaq"
 
 // Built-in LED
 #define PIN_LED 2
 
 bool LED = false;
+
+int RED = 0;
+int GREEN = 0;
+int BLUE = 0;
 
 char XML[2048];
 char buf[32];
@@ -29,13 +33,24 @@ IPAddress subnet(255, 255, 255, 0);
 IPAddress ip;
 
 WebServer server(80);
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   pinMode(PIN_LED, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  
 
   LED = false;
+  REDvalue = 0;
+  GREENvalue = 0;
+  BLUEvalue = 0;
+  
   digitalWrite(PIN_LED, LED);
+  analogWrite(RED, REDvalue);
+  analogWrite(GREEN, GREENvalue);
+  analogWrite(BLUE, BLUEvalue);
   
   disableCore0WDT();
 
@@ -64,13 +79,14 @@ void setup() {
   server.on("/", EnviarSite);
   server.on("/xml", EnviarXML);
   server.on("/LED", ControleLed);
+  server.on("/RGB", ControleRGB);
   server.begin();
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
   server.handleClient();
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void ControleLed() {
   LED = !LED;
   digitalWrite(PIN_LED, LED);
@@ -82,13 +98,17 @@ void ControleLed() {
     server.send(200, "text/plain", "0");
   }
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void ControleRGB(){
+  
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Enviar para o servidor o 'MAIN' (Site em html + css + js) que se encontra definido em 'A1.h'
 void EnviarSite() {
   Serial.println("Enviando PáginaWeb");
   server.send(200, "text/html", MAIN);
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // code to send the main web page
 // I avoid string data types at all cost hence all the char mainipulation code
 void EnviarXML() {
@@ -101,14 +121,30 @@ void EnviarXML() {
   else {
     strcat(XML, "<LED>0</LED>\n");
   }
+
+  //SHOW RGB STATUS
+  const char REDstatus = "<RED>";
+  strcat(REDstatus, REDvalue);
+  strcat(REDstatus, "</RED>");
   
-  strcat(XML, "</Data>\n");
+  const char GREENstatus = "<GREEN>";
+  strcat(GREENstatus, GREENvalue);
+  strcat(GREENstatus, "</GREEN>");
+  
+  const char BLUEstatus = "<BLUE>";
+  strcat(BLUEstatus, BLUEvalue);
+  strcat(BLUEstatus, "</BLUE>");
+  
+
+  strcat(XML, REDstatus);
+  strcat(XML, GREENstatus);
+  strcat(XML, BLUEstatus);
   
   Serial.println(XML);
   
   server.send(200, "text/xml", XML);
 }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void printWifiStatus() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
