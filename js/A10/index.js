a864fdb59dd4145e583c7be45c9c0681cc78d7eb
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2022-11-30 14:19:25 -0300

    Estudando básico de funções

--- js/A10/index.js
+++ js/A10/index.js
@@ -1,6 +1,26 @@
 function funcaoQualquer (parametro){
     console.log(`Escreve qualquer coisa, ${parametro}!`);
+
+    return `Escreve qualquer coisa, ${parametro}!`;
 }
 
 funcaoQualquer();  // Sem parâmetro retorna undefined onde o parâmetro é 'mencionado'
 funcaoQualquer('Feliphe');
+
+console.log(funcaoQualquer('Feliphe')); // executou o log dentro da função além de ter retornado a frase!
+
+function matematica(x=1, y=2){  // Caso não seja indicado parâmetros, estes são os valores padrão
+    return x + y;
+}
+
+console.log(matematica());
+
+console.log(matematica(3, 4));
+
+const raiz = function nomeDaFuncaoEscondido(parametro){  // função anônima
+    return parametro ** 0.5;
+};
+
+const dobro = parametro => parametro * 2; // Arrow function que simplifica tamanho
+
+console.log(dobro(3));
