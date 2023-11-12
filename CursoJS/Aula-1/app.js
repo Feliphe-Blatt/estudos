daac18d55409bdac88d9994c4f08ca7ee090da2e
Feliphe Sousa Blatt <felipheblatt@gmail.com>
2023-11-12 17:38:03 -0300

    Recriando e recomeçando JS

---
+++ CursoJS/Aula-1/app.js
@@ -0,0 +1,8 @@
+console.log('Olá "mundo"!');
+console.log("Olá 'mundo'!");  // O reforço do básico nunca é desperdício
+console.log(`'Olá' "mundo"!`);
+
+/********************************************************************************
+ * console.log('Oi, meu nome é "Feliphe Sousa Blatt" e tenho', 27, 'anos agora!');
+ *******************************************************************************/
+
