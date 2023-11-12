67c250337d35e9b22b8b584147c3b08d35510940
Feliphe Sousa Blatt <felipheblatt@gmail.com>
2023-11-12 18:07:56 -0300

    .

--- CursoJS/Dia1/app.js
+++ CursoJS/Dia1/app.js
@@ -1,8 +1,15 @@
 console.log('Olá "mundo"!');
 console.log("Olá 'mundo'!");  // O reforço do básico nunca é desperdício
-console.log(`'Olá' "mundo"!`);
+console.log(`'Olá' "mundo"!\n`);
 
 /********************************************************************************
  * console.log('Oi, meu nome é "Feliphe Sousa Blatt" e tenho', 27, 'anos agora!');
  *******************************************************************************/
 
+// alert('"Olá!"');  Funciona apenas no navegador!
+
+let nome = 'Feliphe';
+let nomeDoMeio;
+nomeDoMeio = 'Sousa'; //Definindo valor após criação de variável
+let sobreNome = 'Blatt';
+console.log('Nome:', nome, ';\nNome do meio:', nomeDoMeio,';\nSobrenome:',sobreNome,'.\n');
