0f4443aa0b900a26a03866e4ab1c33bd7e573499
Feliphe Sousa Blatt <felipheblatt@gmail.com>
2023-11-13 17:49:53 -0300

    .

--- CursoJS/Dia2/imc.js
+++ CursoJS/Dia2/imc.js
@@ -1,6 +1,11 @@
+const nome = 'Feliphe';
+const nomeDoMeio = 'Sousa';
+const sobreNome = 'Blatt';
+const idade = 27;
 const pesoKg = 70;
 const alturaM = 1.8;
 
-const imc = pesoKg / (alturaM*2);
+const imc = pesoKg / (alturaM * 2);
 
-console.log(imc);
+console.log(`\nEu, ${nome+' '+nomeDoMeio+' '+sobreNome} tenho ${idade} anos;`);
+console.log(`Tenho ${pesoKg}Kg's, ${alturaM}M de altura e meu IMC é: ${imc}!\n`);
