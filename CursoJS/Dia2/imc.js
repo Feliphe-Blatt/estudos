24324812c37b6f6609f603160b62beae6f69d9c4
Feliphe Sousa Blatt <felipheblatt@gmail.com>
2023-11-14 21:28:51 -0300

    .

--- CursoJS/Dia2/imc.js
+++ CursoJS/Dia2/imc.js
@@ -5,7 +5,7 @@ const idade = 27;
 const pesoKg = 70;
 const alturaM = 1.8;
 
-const imc = pesoKg / (alturaM * 2);
+const imc = pesoKg / (alturaM ** 2);
 
 console.log(`\nEu, ${nome+' '+nomeDoMeio+' '+sobreNome} tenho ${idade} anos;`);
 console.log(`Tenho ${pesoKg}Kg's, ${alturaM}M de altura e meu IMC é: ${imc}!\n`);
---
+++ CursoJS/Dia3/aula.js
@@ -0,0 +1,13 @@
+/**
+ * parseInt('1234');
+ * parseFloat("5432");
+ */
+
+alert('Soma de dois números:');
+
+const num1 = Number(prompt('Primeiro número:'));
+const num2 = Number(prompt('Segundo número:'));
+
+const resultado = num1 + num2
+
+alert(`Resultado: ${resultado}`);
