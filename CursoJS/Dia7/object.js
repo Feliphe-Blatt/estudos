edaff0a0c744b1555377ffb1bd391236e7799289
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2023-12-02 17:15:15 -0300

    .
    Organizando arquivos e estudando o básico de objetos.

---
+++ CursoJS/Dia7/object.js
@@ -0,0 +1,21 @@
+
+function addAluno(x, y, z){
+
+    const aluno = {
+        nome : x,
+        idade : y,
+        media : z
+    };
+
+    alert(`${aluno.nome} Adicionado!`);
+    return aluno;
+}
+
+const alunos = [];
+
+alunos.push(addAluno('joao', 18, 8));
+alunos.push(addAluno('maria', 19, 9));
+alunos.push(addAluno('jose', 16, 10));
+alunos.push(addAluno('feliphe', 27, 7));
+
+console.log(alunos);
