e57a44e9784aa8938659ae5954e060009a771dfb
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2023-12-16 14:55:38 -0300

    .

--- CursoJS/tarefas/assets/js/main.js
+++ CursoJS/tarefas/assets/js/main.js
@@ -1,55 +1,72 @@
 function escopo() {
 
-    const lista = document.getElementById('lista');
-    const edit = document.getElementById('edit');
-    const editLista = document.querySelector('.listEdit');
-    const tarefaASerEditada = document.getElementById('taskToEdit');
+    let lista = document.querySelector('#list');
+    const edit = document.querySelector('#edit');
+    let tarefaParaAdd = document.querySelector('#task-to-add');
+    let editLista = document.querySelector('.list-edit');
 
     let flagLista = false;
     let flagEdit = false;
+    
+    tarefaParaAdd.addEventListener('keypress', function (e) {
+        if (e.keyCode === 13) {
+            if (!tarefaParaAdd.value) return;
+            addTarefa(tarefaParaAdd.value);
+        }
+    });
+
+    function addTarefa(tarefa) {
+        const tableRoll = document.createElement('tr');
+        tableRoll.innerHTML = `
+        <td>
+        <h1 class="listItem">
+            "${tarefa}"
+        </h1>
+        </td>
+        <td>
+            <button class="list-edit">Editar</button>
+            <button class="listErase">Apagar</button>
+        </td>
+        `;
+        lista.appendChild(tableRoll);
+    }
 
     function alternaLista(flag) {
         if (flag) {
-            for (let i = 1; i >= 0; i--) {
-                lista.style.opacity = i;
-            }
+            lista.style.opacity = 0;
             flagLista = false;
-
+            lista.style.visibility = 'hidden';
         } else {
-            for (let i = 0; i <= 1; i++) {
-                lista.style.opacity = i;
-            }
+            lista.style.opacity = 1;
             flagLista = true;
+            lista.style.visibility = 'visible';
         }
     }
 
     function alternaEdit(flag) {
         if (flag) {
-            for (let i = 1; i >= 0; i--) {
-                edit.style.opacity = i;
-            }
-            flagEdit = false;
+            edit.style.opacity = 0;
             editLista.innerHTML = 'Editar';
-
+            edit.style.visibility = 'hidden';
+            flagEdit = false;
         } else {
-            for (let i = 0; i <= 1; i++) {
-                edit.style.opacity = i;
-            }
-            flagEdit = true;
+            edit.style.opacity = 1;
             editLista.innerHTML = 'Cancelar';
+            edit.style.visibility = 'visible';
+            flagEdit = true;
         }
     }
 
     document.addEventListener('click', function (event) {
         const element = event.target;
-
-        if (element.classList.contains('editTarefa')) {
-            
+        if (element.classList.contains('add-tarefa')) {
+            if (!tarefaParaAdd.value) return;
+            addTarefa(tarefaParaAdd.value);
         }
         if (element.classList.contains('alternar')) {
             alternaLista(flagLista);
         }
-        if (element.classList.contains('listEdit')) {
+        if (element.classList.contains('list-edit')) {
             alternaEdit(flagEdit);
         }
     });
