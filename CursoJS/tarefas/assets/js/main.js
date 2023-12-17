2cda7a09ff202695e844f78c602b9761317db2c4
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2023-12-17 13:14:13 -0300

    .

--- CursoJS/tarefas/assets/js/main.js
+++ CursoJS/tarefas/assets/js/main.js
@@ -1,41 +1,53 @@
 function escopo() {
 
-    let lista = document.querySelector('#list');
-    const edit = document.querySelector('#edit');
-    let tarefaParaAdd = document.querySelector('#task-to-add');
-    let editLista = document.querySelector('.list-edit');
+    ///////////////////////////////////////////////////////////////////////////  Adicionando tarefa
 
-    let flagLista = false;
-    let flagEdit = false;
-    
-    tarefaParaAdd.addEventListener('keypress', function (e) {
-        if (e.keyCode === 13) {
-            if (!tarefaParaAdd.value) return;
-            addTarefa(tarefaParaAdd.value);
-        }
-    });
 
-    function addTarefa(tarefa) {
+    function escreveTarefa(tarefa, i) {  //  Escrevendo tarefa no documento html
+
+        if (typeof i === 'undefined') i = 'x';  //  Definindo valor padrão para 'i' caso não seja passado valor
+
         const tableRoll = document.createElement('tr');
         tableRoll.innerHTML = `
         <td>
-        <h1 class="listItem">
-            "${tarefa}"
-        </h1>
+            <h1 class="listItem" id="item-${i}">
+                ${tarefa}
+            </h1>
         </td>
         <td>
-            <button class="list-edit">Editar</button>
-            <button class="listErase">Apagar</button>
+            <button class="list-edit" id="btn-edit-${i}" title="Editar a tarefa">Editar</button>
+            <button class="list-erase" id="btn-erase-${i}" title="Apagar a tarefa">Apagar</button>
         </td>
         `;
-        lista.appendChild(tableRoll);
+        const corpo = document.querySelector('#lista-de-tarefas');
+        corpo.appendChild(tableRoll);
     }
 
+    function limpaInput() {
+        tarefaParaAdd.value = '';
+        tarefaParaAdd.focus();
+    }
+
+    function addTarefa(tarefa) {
+        escreveTarefa(tarefa, tarefas.length);
+        tarefas.push(tarefa);
+        limpaInput();
+    }
+
+    /////////////////////////////////////////////////////////////////////////// Apaga a tarefa
+    function apagarTarefa(element) {
+
+        const botoes = element.parentElement;
+        botoes.parentElement.remove();
+    }
+
+    /////////////////////////////////////////////////////////////////////////// Alternam visão dos elementos html
     function alternaLista(flag) {
         if (flag) {
             lista.style.opacity = 0;
             flagLista = false;
             lista.style.visibility = 'hidden';
+
         } else {
             lista.style.opacity = 1;
             flagLista = true;
@@ -46,28 +58,51 @@ function escopo() {
     function alternaEdit(flag) {
         if (flag) {
             edit.style.opacity = 0;
-            editLista.innerHTML = 'Editar';
             edit.style.visibility = 'hidden';
             flagEdit = false;
         } else {
             edit.style.opacity = 1;
-            editLista.innerHTML = 'Cancelar';
             edit.style.visibility = 'visible';
             flagEdit = true;
         }
     }
 
-    document.addEventListener('click', function (event) {
-        const element = event.target;
-        if (element.classList.contains('add-tarefa')) {
+    ///////////////////////////////////////////////////////////////////////////  Variáveis e eventos
+
+    const lista = document.querySelector('#list');
+    const edit = document.querySelector('#edit');
+    const tarefaParaAdd = document.querySelector('#task-to-add');
+    const editLista = document.querySelector('.list-edit');
+
+    let flagLista = true;
+    let flagEdit = false;
+    let tarefas = [];
+
+    tarefaParaAdd.addEventListener('keypress', function (e) {  // Quando 'enter' for pressionado na adição da tarefa
+        if (e.keyCode === 13) {
             if (!tarefaParaAdd.value) return;
             addTarefa(tarefaParaAdd.value);
         }
-        if (element.classList.contains('alternar')) {
+    });
+
+    document.addEventListener('click', function (event) {
+        const element = event.target;
+
+        if (element.classList.contains('add-tarefa')) {  // Adição de tarefa
+            if (!tarefaParaAdd.value) return;
+            addTarefa(tarefaParaAdd.value);
+        }
+        if (element.classList.contains('list-alt')) {  // Alternar visão da lista
             alternaLista(flagLista);
         }
-        if (element.classList.contains('list-edit')) {
+        if (element.classList.contains('list-edit')) {  // Edição da tarefa
+            if (element.id === 'btn-edit-x') return;  //  Nada acontece com a tarefa X
             alternaEdit(flagEdit);
+        }
+        if (element.classList.contains('list-erase')) {  // Apagar tarefa
+            if (flagEdit === true) alternaEdit(flagEdit);
+            if (element.id === 'btn-erase-x') return;  //  Nada acontece com a tarefa X
+            apagarTarefa(element);
         }
     });
 }
