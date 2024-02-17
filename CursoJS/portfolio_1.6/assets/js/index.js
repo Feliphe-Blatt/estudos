5e61cb1a2bd78f902382b062e85a80811efc5cf2
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2024-02-17 20:40:14 -0300

    .

---
+++ CursoJS/portfolio_1.6/assets/js/index.js
@@ -0,0 +1,33 @@
+function escopo() {
+    document.addEventListener("DOMContentLoaded", function () {
+        const navInicio = document.getElementById("nav-inicio");
+        const navGestaoTarefas = document.getElementById("nav-gestaoTarefas");
+        const navRedirectPage = document.getElementById("nav-redirectPage");
+        const navContato = document.getElementById("nav-contato");
+
+        function trocaSite(event) {
+            event.preventDefault();
+
+            const linkId = event.target.getAttribute("id");
+
+            if (linkId === "nav-inicio") {
+                console.clear();
+                console.log("Início");
+
+            } else if (linkId === "nav-gestaoTarefas") {
+                console.clear();
+                console.log("Gestão de Tarefas");
+
+            } else if (linkId === "nav-redirectPage") {
+                console.clear();
+                console.log("Redirect Page");
+                
+            }
+        }
+
+        navInicio.addEventListener("click", trocaSite);
+        navGestaoTarefas.addEventListener("click", trocaSite);
+        navRedirectPage.addEventListener("click", trocaSite);
+    });
+}
+escopo();
