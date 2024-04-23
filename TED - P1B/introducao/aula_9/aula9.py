01af6bff62e19beb0b686075ca0ee8671ece1d21
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2024-04-22 21:31:13 -0300

    Exercise: update dos arquivos de estudos.

---
+++ TED - P1B/introducao/aula_9/aula9.py
@@ -0,0 +1,10 @@
+import customtkinter
+
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('dark-blue')
+
+app = customtkinter.CTk()
+app.geometry("600x600")
+app.title("nome da janela")
+
+app.mainloop()
