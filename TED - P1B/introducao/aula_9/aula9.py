e855d4b8e6a5d353c8f1546ef5b18fcf8c7c1b9f
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2024-04-29 19:55:50 -0300

    exercise: venv e customtkinter

--- TED - P1B/introducao/aula_9/aula9.py
+++ TED - P1B/introducao/aula_9/aula9.py
@@ -7,4 +7,7 @@ app = customtkinter.CTk()
 app.geometry("600x600")
 app.title("nome da janela")
 
+mensagem_inicial = customtkinter.CTkLabel(app, text='Alou Rapeize...')
+mensagem_inicial.pack(padx=10, pady=10)
+
 app.mainloop()
