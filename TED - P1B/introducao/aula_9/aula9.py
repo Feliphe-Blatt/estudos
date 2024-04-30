7cfad9c8e79d2a2a6f95e526997c5e18a13ed89e
Feliphe Sousa Blatt <106674685+Feliphe-Blatt@users.noreply.github.com>
2024-04-29 21:11:56 -0300

    Exercise: customtkinter

--- TED - P1B/introducao/aula_9/aula9.py
+++ TED - P1B/introducao/aula_9/aula9.py
@@ -1,5 +1,8 @@
 import customtkinter
 
+
+fonte_app = customtkinter.CTkFont(family="Helvetica", size=20)
+
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('dark-blue')
 
@@ -7,7 +10,22 @@ app = customtkinter.CTk()
 app.geometry("600x600")
 app.title("nome da janela")
 
-mensagem_inicial = customtkinter.CTkLabel(app, text='Alou Rapeize...')
+mensagem_inicial = customtkinter.CTkLabel(app, text='Alou Rapeize...', font=fonte_app)
 mensagem_inicial.pack(padx=10, pady=10)
+
+val_1 = customtkinter.StringVar()
+input = customtkinter.CTkEntry(app, placeholder_text = 'Digite algo...', textvariable = val_1)
+input.pack(padx=10, pady=10)
+
+temporario = ''
+
+def somar():
+
+    
+
+botao = customtkinter.CTkButton(app, text='Calcular', command=somar)
+
+mensagem_final = customtkinter.CTkLabel(app, text='Resultado:', font=fonte_app)
+mensagem_final.pack(padx=10, pady=10)
 
 app.mainloop()
